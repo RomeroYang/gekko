3b9feae8eb82ec490ae1aa5b4bbcc7287e089049
RomeroYang <yangxy@365qian.com>
2017-06-16 06:21:36 +0800

    add supershort st with boll&rsi
    add supershort st with boll&rsi

--- strategies/custom.js
+++ strategies/custom.js
@@ -9,6 +9,8 @@
 // long or short).
 
 var log = require('../core/log');
+// var ta = require('./ta/my');
+var ss = require('./ta/super_short');
 
 // Let's create our own strat
 var strat = {};
@@ -17,22 +19,40 @@ var strat = {};
 strat.init = function() {
   this.currentTrend = 'long';
   this.requiredHistory = 0;
+
+  this.addIndicator('rsi', 'RSI', {interval: 14})
+  // this.addIndicator('dema', 'DEMA', {short: 10, long: 21})
+  // this.addIndicator('ppo', 'PPO', {short: 12, long: 26, signal: 9})
+  this.addIndicator('macd', 'MACD', {short: 14, long: 21, signal: 9})
 }
 
 // What happens on every new candle?
 strat.update = function(candle) {
+  candle.rsi = this.indicators.rsi.rsi;
+  // var dema = this.indicators.dema;
+  // var ppo = this.indicators.ppo;
+  candle.macd = this.indicators.macd.result;
+  // var trend = ta.update({rsi, dema, ppo, macd});
+  var trend = ss.update(candle);
 
-  // Get a random number between 0 and 1.
-  this.randomNumber = Math.random();
-
-  // There is a 10% chance it is smaller than 0.1
-  this.toUpdate = this.randomNumber < 0.1;
+  // this.currentTrend = trend;
+  if (trend == 'none') {
+    this.toUpdate = false;
+    return;
+  }
+  // console.log('trend:'+trend+'currentTrend:'+this.currentTrend)
+  if (trend != this.currentTrend) {
+    console.log('toUpdate:'+trend)
+    this.toUpdate = true;
+  } else {
+    this.toUpdate = false;
+  }
 }
 
 // For debugging purposes.
 strat.log = function() {
-  log.debug('calculated random number:');
-  log.debug('\t', this.randomNumber.toFixed(3));
+  log.debug('currentTrend:');
+  log.debug('\t', this.currentTrend);
 }
 
 // Based on the newly calculated
@@ -57,6 +77,12 @@ strat.check = function() {
     this.advice('long');
 
   }
+
+  // if (this.currentTrend === 'none') {
+  //   return;
+  // } else {
+  //   this.advice(this.currentTrend);
+  // }
 }
 
 module.exports = strat;
---
+++ strategies/ta/super_short.js
@@ -0,0 +1,100 @@
+var ss = {}
+var his = []
+const wnd = 10
+let signal_buy = 0
+let signal_sell = 0
+
+ss.update = candle => {
+    const {high, low, close, macd, rsi} = candle
+    let item = {high, low, close, macd, rsi}
+    his[his.length] = item
+
+    // console.log(`high${high.toFixed(2)}low${low.toFixed(2)}close${close.toFixed(2)}macd${macd.toFixed(2)}`)
+    if (his.length > wnd) {
+        his.shift()
+    } else {
+        return 'none'
+    }
+    // console.log(his.length)
+
+    const ma = calcAverage(his)
+    const std = calcStd(his, ma)
+    const boll = calcBollIndex(his, ma, std)
+
+    // console.log(`ma:${ma} std:${std} boll_u:${boll.up} boll_d:${boll.down} boll_std:${boll.std_ratio}`)
+    if (boll.ratio > boll.std_ratio) {
+        console.log(`price in high: diff-ratio-${(boll.ratio).toFixed(4)}`)
+        item.boll_ratio_h = boll.ratio - boll.std_ratio
+    } else if (boll.ratio < (0-boll.std_ratio)) {
+        console.log(`price in low: diff-ratio-${(boll.ratio).toFixed(4)}`)
+        item.boll_ratio_l = boll.ratio + boll.std_ratio
+    } else {
+        if (boll.std_ratio > 0.0035) {
+            if (his[his.length-2].boll_ratio_h && rsi > 75) {
+                console.log(`find short point: diff-ratio-${(boll.ratio).toFixed(4)}`)
+                signal_sell ++
+                signal_buy = 0
+                if (signal_sell > 0) {
+                    signal_sell = 0
+                    return 'short'
+                }
+            }
+            if (his[his.length-2].boll_ratio_l && rsi < 30) {
+                console.log(`find long point: diff-ratio-${(boll.ratio).toFixed(4)}`)
+                signal_buy ++
+                signal_sell = 0
+                if (signal_buy > 0) {
+                    signal_buy = 0
+                    return 'long'
+                }
+            }
+        }
+    }
+    // console.log(`boll_ratio:${boll.ratio}`)
+
+    // const h30 = his.reduce((a, b) => {
+    //     return a>b.high?a:b.high
+    // }, 0)
+    // const l30 = his.reduce((a, b) => {
+    //     return a<b.low?a:b.low
+    // }, 0)
+    // const macd30h = his.reduce((a, b) => {
+    //     return a.macd>b.macd?a:b
+    // })['macd']
+    // const macd30l = his.reduce((a, b) => {
+    //     return a.macd<b.macd?a:b
+    // })['macd']
+    // if (macd30h == his[his.length - 2]['macd']) {
+    //     return 'short'
+    // }
+    // if (macd30l == his[his.length - 2]['macd']) {
+    //     return 'long'
+    // }
+
+    return 'none'
+}
+
+const calcAverage = his => {
+    return his.reduce((a, b) => a+(b.close-0), 0) / his.length
+}
+const calcStd = (his, average) => {
+    return Math.sqrt(his.map(item => Math.pow(item.close-average, 2)).reduce((a, b) => a+(b-0), 0) / his.length)
+}
+const calcBollIndex = (his, ma, std) => {
+    let boll = {}
+    boll.up = ma + (std * 2)
+    boll.down = ma - (std * 2)
+    boll.std_ratio = (std * 2) / ma
+    boll.ratio = his[his.length - 1].close / ma -1
+    return boll
+}
+
+const checkRSI = rsi => {
+    const h = 75
+    const l = 33
+    if (rsi.rsi > h) return 'short'
+    if (rsi.rsi < l) return 'long'
+    return 'none'
+}
+
+module.exports = ss;
